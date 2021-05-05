1ad40e9492097bf2e925db36b3cb4170db284bd4
Ximan Mao <K31870@eltham-college.org.uk>
2021-05-05 11:20:37 +0100

    Add project files.

---
+++ ConsoleApp1/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Node
+    {
+        public Node left;
+        public Node right;
+        public int val;
+
+
+    }
+}
